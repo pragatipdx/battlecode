59f41c78bdc260b426a4d94d41282cd28bfcb292
Sam Huebschwerlen <huebschwerlen@gmail.com>
2021-10-20 09:43:14 -0700

    adding muckraker to master

--- src/BrigadePlayer/RobotPlayer.java
+++ src/BrigadePlayer/RobotPlayer.java
@@ -2,6 +2,10 @@ package BrigadePlayer;
 
 import battlecode.common.*;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public strictfp class RobotPlayer {
     static RobotController rc;
 
@@ -25,6 +29,10 @@ public strictfp class RobotPlayer {
     static int turnCount;
     static MapLocation ec_Location;
     static Direction target;
+
+    //THESE NEED TO BE CHANGED
+    static MapLocation homeLoc = null;
+    static int homeID = 0;
 
     /**
      * run() is the method that is called when a robot is instantiated in the Battlecode world.
@@ -141,24 +149,103 @@ public strictfp class RobotPlayer {
 
     }
 
+
+
     static void runMuckraker() throws GameActionException {
         Team enemy = rc.getTeam().opponent();
-        int actionRadius = rc.getType().actionRadiusSquared;
+        Team friend = rc.getTeam();
 
+        int actionRadius = 12;
+        int senseRadius = 30;
+        int detectionRadius = 40;
+        int detectionRadiusSqr = rc.getType().detectionRadiusSquared;
+
+        System.out.println("\nDetection Radius const: 40    DetectionRadius Squared: " + detectionRadiusSqr);
+
+        System.out.println("\n Sensing Nearby Friendly Bots... " + "\n");
+        for (RobotInfo robot : rc.senseNearbyRobots(actionRadius,friend) ) {
+            System.out.println("\nRobot type: " + robot.type + " Robot Id: "
+                    + robot.ID + " Robot Loc: " +  robot.location + "\n");
+            if (robot.type.equals(RobotType.ENLIGHTENMENT_CENTER)) {
+                homeLoc = robot.location;
+                homeID = robot.ID;
+                System.out.println("Found Home! ID: " + homeID + " LOC: " + homeLoc);
+                break;
+            }
+        }
+
+        //detect ALL nearby robots and see how close/far they are
+        System.out.println("\n Detecting ALL Nearby Bots... " + "\n");
+
+        for (MapLocation location : rc.detectNearbyRobots() ) {
+
+            //lower the number returned, farther the distance away
+            int distance = rc.getLocation().compareTo(location);
+
+            System.out.println("\nRobot detected at : " + location + "\nMy Location: " + rc.getLocation() +
+                    "Distance: " + distance + "\n");
+        }
+
+        //sanity check //print info about self
+        System.out.println("\nHome: ID: " + homeID + " LOC: " + homeLoc + " TurnCount: " + turnCount
+                + " Player Cooldown: " + rc.getCooldownTurns() + " Player action/sense/detect radi: " +
+                actionRadius + " / " + senseRadius + " / " + detectionRadius + "\n");
+
+        System.out.println("TEST1");
+
+        //look for robots in action radius and expose them if possible
         for (RobotInfo robot : rc.senseNearbyRobots(actionRadius, enemy)) {
-
             if (robot.type.canBeExposed()) {
                 // It's a slanderer... go get them!
                 if (rc.canExpose(robot.location)) {
-                    System.out.println("e x p o s e d");
+                    System.out.println("\ne x p o s e d\n");
                     rc.expose(robot.location);
-
-                    return;
+                    return;//why return instead of moving after?not possible?
                 }
             }
         }
-        if (tryMove(randomDirection()))
-            System.out.println("I moved!");
+
+        System.out.println("TEST2");
+
+        //sense nearby enemies
+        for (RobotInfo robot : rc.senseNearbyRobots(senseRadius, enemy)) {
+            System.out.println("This loop should only run if enemy is detected");
+
+            if (robot.type.equals(RobotType.ENLIGHTENMENT_CENTER)) {
+                // It's an Enemy EC! Make your way there to SWARM
+                System.out.println("\nFOUND ENEMY EC at : " + robot.location);
+                basicBug(robot.location);
+                System.out.println("\nMOVED/MOVING TOWARD ENEMY EC");
+                break;
+            } else if (robot.type.equals(RobotType.SLANDERER)) {
+                // It's a slanderer! advance to EXPOSE!
+                System.out.println("\nFOUND ENEMY BOT at : " + robot.location);
+                basicBug(robot.location);
+                System.out.println("\nMOVED/MOVING TOWARD ENEMY BOT");
+                break;
+            } else if (robot.type.equals(RobotType.MUCKRAKER)) {
+                // It's a slanderer! advance to EXPOSE!
+                System.out.println("\nFOUND ENEMY BOT at : " + robot.location);
+                basicBug(robot.location);
+                System.out.println("\nMOVED/MOVING TOWARD ENEMY BOT");
+                break;
+            } else  {
+                // It's an Enemy Politician! Avoid them lest lose your conviction
+                System.out.println("\nFOUND ENEMY BOT at : " + robot.location);
+
+                //needs to IMPLEMENT!!
+                if (runAway(robot.location)){
+                    System.out.println("\nMOVED/MOVING AWAY FROM ENEMY POLITICIAN");
+                }
+
+                return;
+
+            }
+
+        }
+
+        if (tryMoveAwayFromHome())
+            System.out.println("Tally Ho!");
     }
 
     /**
@@ -212,5 +299,119 @@ public strictfp class RobotPlayer {
 
         return false;
     }
+
+
+    static boolean tryMoveAwayFromHome() throws GameActionException {
+        System.out.println("Trying to move away from home");
+        double bestOption = 0.0;
+        double possiblePass = 0.0;
+        Direction homeDir = rc.getLocation().directionTo(homeLoc);
+        Direction tempDir = randomDirection();
+        System.out.println("Home dir: " + homeDir);
+        List<Direction> bestPossDirs = new ArrayList<>();
+        for (Direction possibleDir : directions) {
+            if (rc.canMove(possibleDir)) {
+                System.out.println("TESTTT");
+                possiblePass = rc.sensePassability(rc.getLocation().add(possibleDir));
+            } else {
+                continue;
+            }
+
+            System.out.println("Checking all possible directions....");
+            if (rc.canMove(possibleDir) && possibleDir != homeDir && possiblePass > 0.2) {
+//                possiblePass = rc.sensePassability(rc.getLocation().add(possibleDir));
+//                System.out.println("Possible pass: " + possiblePass + " at " + possibleDir + "; Current Best option: " + bestOption);
+                System.out.println("Possible pass: " + possiblePass + " at " + possibleDir + " ...adding to list");
+                bestPossDirs.add(possibleDir);
+//                if (possiblePass > bestOption) {
+//                    bestOption = possiblePass;
+//                    tempDir = possibleDir;
+//                    System.out.println("\nFound better option: " + bestOption + " at " + possibleDir);
+            }
+        }
+        if (bestPossDirs.isEmpty()) {
+            tempDir = randomDirection();
+        } else {
+            Collections.shuffle(bestPossDirs);
+            tempDir = bestPossDirs.get(0);
+        }
+        System.out.println("I am trying to move " + tempDir + "; Is ready? " + rc.isReady() + " Cooldown Turns:" + rc.getCooldownTurns() + " CanMove in Dir?" + rc.canMove(tempDir));
+        if (rc.canMove(tempDir)) {
+            rc.move(tempDir);
+            return true;
+        } else return false;
+    }
+
+    static boolean runAway(MapLocation location) throws GameActionException {
+        System.out.println("Trying to move away from Enemy");
+        double bestOption = 0.0;
+        double possiblePass = 0.0;
+        Direction homeDir = rc.getLocation().directionTo(location);
+        Direction tempDir = randomDirection();
+        System.out.println("Enemy dir: " + homeDir);
+        List<Direction> bestPossDirs = new ArrayList<>();
+        for (Direction possibleDir : directions) {
+            if (rc.canMove(possibleDir)) {
+                System.out.println("TEST");
+                possiblePass = rc.sensePassability(rc.getLocation().add(possibleDir));
+            } else {
+                continue;
+            }
+
+            System.out.println("Checking all possible directions....");
+            if (rc.canMove(possibleDir) && possibleDir != homeDir && possiblePass > 0.2) {
+//                possiblePass = rc.sensePassability(rc.getLocation().add(possibleDir));
+//                System.out.println("Possible pass: " + possiblePass + " at " + possibleDir + "; Current Best option: " + bestOption);
+                System.out.println("Possible pass: " + possiblePass + " at " + possibleDir + " ...adding to list");
+                bestPossDirs.add(possibleDir);
+//                if (possiblePass > bestOption) {
+//                    bestOption = possiblePass;
+//                    tempDir = possibleDir;
+//                    System.out.println("\nFound better option: " + bestOption + " at " + possibleDir);
+            }
+        }
+        if (bestPossDirs.isEmpty()) {
+            tempDir = randomDirection();
+        } else {
+            Collections.shuffle(bestPossDirs);
+            tempDir = bestPossDirs.get(0);
+        }
+        System.out.println("I am trying to move " + tempDir + "; Is ready? " + rc.isReady() + " Cooldown Turns:" + rc.getCooldownTurns() + " CanMove in Dir?" + rc.canMove(tempDir));
+        if (rc.canMove(tempDir)) {
+            rc.move(tempDir);
+            return true;
+        } else return false;
+    }
+
+
+    //basic pathfinding bug
+    //NEEDS TO BE TESTED - HAS NOT BEEN VERIFIED
+    static final double passabilityThreshold = 0.6;
+    static Direction bugDirection = null;
+    static void basicBug(MapLocation target) throws GameActionException {
+        Direction dir = rc.getLocation().directionTo(target);
+        if (rc.getLocation().equals(target)) {
+            //do something
+        } else {
+            if (bugDirection == null) {
+                bugDirection = dir.rotateRight();
+            }
+            for (int i = 0; i < 8; ++i) {
+                if (rc.canMove(bugDirection) && rc.sensePassability(rc.getLocation().add(bugDirection)) >= passabilityThreshold) {
+                    rc.move(bugDirection);
+                    break;
+                }
+                bugDirection = bugDirection.rotateRight();
+            }
+            bugDirection = bugDirection.rotateLeft();
+        }
+    }
+
+
+
+
+
+
+
 
 }
