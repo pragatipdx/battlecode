d92a4432520176779513e694602596edd7f726b5
Sam Huebschwerlen <huebschwerlen@gmail.com>
2021-11-02 13:27:33 -0700

    add test template

--- test/examplefuncsplayer/RobotPlayerTest.java
+++ test/examplefuncsplayer/RobotPlayerTest.java
@@ -1,13 +1,43 @@
 package examplefuncsplayer;
 
+import battlecode.common.*;
+import org.mockito.*;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class RobotPlayerTest {
 
+	@Mock
+	RobotInfo[] tempRobotInfo;
+	@Mock
+//	your rcmock should be assigned with the RobotController mock
+	RobotController RCmock = mock(RobotController.class);
+
+
+//	// mock creation
+//	List mockedList = mock(List.class);
+//
+//	// using mock object - it does not throw any "unexpected interaction" exception
+//	mockedList.add("one");
+//	mockedList.clear();
+//
+//	// selective, explicit, highly readable verification
+//	verify(mockedList).add("one");
+//	verify(mockedList).clear();
+
 	@Test
 	public void testSanity() {
 		assertEquals(2, 1+1);
+	}
+
+	@Test
+	public void testSenseNearbyRobots() {
+		when(RCmock.senseNearbyRobots()).thenReturn(tempRobotInfo);
+//		assertArrayEquals(tempRobotInfo, RCmock.senseNearbyRobots());
+
 	}
 
 }
