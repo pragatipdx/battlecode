2d71a132d8e252cd8c5121e48522e0051b3fe3e4
Alvin-PDX <iskender@pdx.edu>
2021-11-18 08:03:29 -0800

    Moved EC test to its own file

---
+++ test/BrigPlayerV3/ECTest.java
@@ -0,0 +1,34 @@
+package BrigPlayerV3;
+
+import static org.junit.Assert.*;
+
+import battlecode.common.*;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class ECTest {
+
+    private RobotController rcTest = mock(RobotController.class);
+    private EnlightenmentCenter ECtest = new EnlightenmentCenter(rcTest);
+
+
+
+    //test that enemy politicians in sense radius are counted correctly
+    @Test
+    public void testCountEnemyPoliticians() throws GameActionException {
+        RobotInfo bot1 = new RobotInfo(1, Team.B, RobotType.POLITICIAN, 10, 10, new MapLocation(0, 0));
+        RobotInfo bot2 = new RobotInfo(2, Team.A, RobotType.POLITICIAN, 10, 10, new MapLocation(1, 0));
+        RobotInfo[] arr1 = new RobotInfo[2];
+        arr1[0] = bot1;
+        arr1[1] = bot2;
+        when(rcTest.senseNearbyRobots()).thenReturn(arr1);
+        when(rcTest.getTeam()).thenReturn(Team.A);
+
+        assertEquals(1, ECtest.countEnemyPoliticians());
+    }
+}
