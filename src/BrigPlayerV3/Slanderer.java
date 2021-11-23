eed9a39f3873ae8fe9f3fd506ffcfe7c7f9ef324
larrisacarvalho <larris2@pdx.edu>
2021-11-23 13:18:00 -0800

    Slanderer test modification

--- src/BrigPlayerV3/Slanderer.java
+++ src/BrigPlayerV3/Slanderer.java
@@ -2,8 +2,16 @@ package BrigPlayerV3;
 
 import battlecode.common.*;
 
-public class Slanderer extends Unit {
+import java.util.ArrayList;
+import java.util.List;
 
+public class Slanderer extends Unit {
+    List<RobotInfo> nearbyEnemyMuckraker = new ArrayList<>();
+    List<RobotInfo> nearbyEC = new ArrayList<>();
+    List<RobotInfo> nearbyEnemies = new ArrayList<>();
+    List<RobotInfo> nearbyAlly = new ArrayList<>();
+    int senseRadius = 30;
+    MapLocation currentLocation;
     public Slanderer(RobotController r) {
         super(r);
     }
@@ -15,16 +23,44 @@ public class Slanderer extends Unit {
     public void takeTurn() throws GameActionException {
         super.takeTurn();
         MapLocation location = rc.getLocation();
-        runFromEnemy(location);
-        stayCLoseToHome(location);
+        RobotInfo[] robotInfosInSenseRadius = senseNearbyRobotsInSenseRadius();
+        if(robotInfosInSenseRadius.length > 0){
+
+            nearbyEnemies = getNearbyEnemies(robotInfosInSenseRadius);
+            populateEnemyLists();
+            nearbyAlly = getNearbyAlly(robotInfosInSenseRadius);
+            populateAllyLists();
+        }
+        runFromEnemy();
+        stayCLoseToHome();
 
         if (nav.tryMove(Utility.randomDirection()))
             System.out.println("I moved!");
     }
+    RobotInfo[] senseNearbyRobotsInSenseRadius() {
+        RobotInfo[] robotInfos = rc.senseNearbyRobots(senseRadius);
+        return robotInfos;
+    }
+    Boolean populateEnemyLists() {
+        if(!nearbyEnemies.isEmpty()) {
+            clearPreexistingLists();
+            addRobotsToLists();
+            return true;
+        } else
+            return false;
+    }
+    Boolean populateAllyLists() {
+        if(!nearbyAlly.isEmpty()) {
+            clearPreexistingLists();
+            addRobotsToLists();
+            return true;
+        } else
+            return false;
+    }
 
-    public boolean runFromEnemy(MapLocation avoid) throws GameActionException {
-        final int senseRadius = 20;
-        try{
+    public boolean runFromEnemy() throws GameActionException {
+        /*try{
+
         Team enemy = rc.getTeam().opponent();
         for (RobotInfo robot : rc.senseNearbyRobots(senseRadius, enemy)) {
             if (robot.getType() == RobotType.MUCKRAKER) {
@@ -34,18 +70,26 @@ public class Slanderer extends Unit {
                 }
             }
         }
-
-    }
-        catch(NullPointerException e){
+    //}
+       /* catch(NullPointerException e){
             System.out.println("NullPointerException thrown!");
 
     }
-        return true;
+        return true;*/
+        if(!nearbyEnemyMuckraker.isEmpty()) {
+            for (RobotInfo robot : nearbyEnemyMuckraker) {
+                System.out.println("\nFound Enemy MUCKRACKER!!!!! -   MOVING   AWAY!!!!    DISTANCE: " + currentLocation.distanceSquaredTo(robot.getLocation()));
+                nav.moveAway(robot.location);
+            }
+            return true;
+        }
+        return false;
+
     }
 
 
-    public boolean stayCLoseToHome(MapLocation avoid) throws GameActionException {
-        Team ally = rc.getTeam();
+    public boolean stayCLoseToHome() throws GameActionException {
+        /*Team ally = rc.getTeam();
         int actionRadiusA = rc.getType().actionRadiusSquared;
         try {
         for (RobotInfo robotA : rc.senseNearbyRobots(actionRadiusA, ally)) {
@@ -65,5 +109,48 @@ public class Slanderer extends Unit {
             }
 
 
+        return true; */
+        if(!nearbyEC.isEmpty()) {
+            for (RobotInfo robot : nearbyEC) {
+                System.out.println("\nFound Friend EC - MOVING CLOSER    DISTANCE: " + currentLocation.distanceSquaredTo(robot.getLocation()));
+                nav.goTo(robot.location);
+            }
+            return true;
+        } else
+            return false;
+    }
+    List<RobotInfo> getNearbyEnemies(RobotInfo[] robotInfos) {
+        List<RobotInfo> nearbyEnemies = new ArrayList<>();
+        for (RobotInfo robot : robotInfos) {
+            if (robot.team.equals(enemy)) {
+                nearbyEnemies.add(robot);
+            }
+        }
+        return nearbyEnemies;
+    }
+    List<RobotInfo> getNearbyAlly(RobotInfo[] robotInfos) {
+        List<RobotInfo> nearbyAlly = new ArrayList<>();
+        for (RobotInfo robot : robotInfos) {
+            if (robot.team.equals(friend)) {
+                nearbyAlly.add(robot);
+            }
+        }
+        return nearbyAlly;
+    }
+
+    private void addRobotsToLists() {
+        for (RobotInfo robot : nearbyAlly) {
+            if (robot.type.equals(RobotType.ENLIGHTENMENT_CENTER)) {
+                nearbyEC.add(robot);
+            }
+            if (robot.type.equals(RobotType.MUCKRAKER)) {
+                nearbyEnemyMuckraker.add(robot);
+            }
+        }
+    }
+    Boolean clearPreexistingLists() {
+        nearbyEnemyMuckraker.clear();
+        nearbyEC.clear();
         return true;
-    }}
+    }
+}
--- test/BrigPlayerV3/SlandererTest.java
+++ test/BrigPlayerV3/SlandererTest.java
@@ -1,10 +1,14 @@
 package BrigPlayerV3;
 
 import battlecode.common.*;
+import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
+import java.util.ArrayList;
+
+import static org.junit.Assert.*;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
@@ -15,11 +19,95 @@ public class SlandererTest  {
 
     private RobotController rcTest = mock(RobotController.class);
     private Slanderer slandererTest = new Slanderer(rcTest, enemy, friend);
+    int senseRadius = 30;
 
+    RobotInfo enemyMuck = new RobotInfo(2, enemy, RobotType.MUCKRAKER, 10, 10, new MapLocation(1, 0));
 
-    RobotInfo enemyBotMuck = new RobotInfo(2, enemy, RobotType.MUCKRAKER, 10, 10, new MapLocation(1, 0));
+    RobotInfo friendBotEC = new RobotInfo(1, friend, RobotType.ENLIGHTENMENT_CENTER, 10, 10, new MapLocation(1, 0));
+    RobotInfo enemyBotPoli = new RobotInfo(2, enemy, RobotType.POLITICIAN, 10, 10, new MapLocation(1, 0));
+   // RobotInfo enemyBotMuck = new RobotInfo(2, enemy, RobotType.MUCKRAKER, 10, 10, new MapLocation(1, 0));
+    RobotInfo friendBotSland = new RobotInfo(1, friend, RobotType.SLANDERER, 10, 10, new MapLocation(0, 0));
+    RobotInfo enemyBotSland = new RobotInfo(2, enemy, RobotType.SLANDERER, 10, 10, new MapLocation(1, 0));
+    RobotInfo[] friendBotArr = new RobotInfo[2];
+    RobotInfo[] enemyBotArr = new RobotInfo[2];
+    ArrayList<RobotInfo> nearbyEnemyMuckrakerNotMock = new ArrayList<>();
+    ArrayList<RobotInfo> nearbyAllyECNotMock = new ArrayList<>();
+
+    @Mock
+    private ArrayList<RobotInfo> nearbyEnemyMuckraker;
+    private RobotInfo[] EnemyBotInfoArr = new RobotInfo[1];
+    private ArrayList<RobotInfo> nearbyEC;
+    private RobotInfo[] AllyInfoArr = new RobotInfo[1];
+
+    @Before
+    public void setUp() {
+        MockitoAnnotations.initMocks(this);
+    }
+
 
     @Test
+    public void senseNearbyRobotsTestNullArrayE() throws GameActionException {
+        RobotInfo[] robots = {};
+        when(rcTest.senseNearbyRobots(senseRadius, enemy)).thenReturn(robots);
+
+        assertArrayEquals(null, slandererTest.senseNearbyRobotsInSenseRadius());
+    }
+
+    @Test
+    public void senseNearbyRobotsTestNullArrayF() throws GameActionException {
+        RobotInfo[] robots = {};
+        when(rcTest.senseNearbyRobots(senseRadius, friend)).thenReturn(robots);
+
+        assertArrayEquals(null, slandererTest.senseNearbyRobotsInSenseRadius());
+    }
+
+    @Test
+    public void senseNearbyRobotsTestArrayLen() throws GameActionException {
+        friendBotArr[0] =friendBotEC;
+
+        when(rcTest.senseNearbyRobots(senseRadius)).thenReturn(friendBotArr);
+
+        assertEquals(friendBotArr.length, slandererTest.senseNearbyRobotsInSenseRadius().length);
+        assertArrayEquals(friendBotArr, slandererTest.senseNearbyRobotsInSenseRadius());
+    }
+
+    @Test
+    public void handleEnemyMuckFalse() throws GameActionException {
+        assertFalse(slandererTest.runFromEnemy());
+    }
+
+    @Test
+    public void handleAllyEC() throws GameActionException {
+        assertFalse(slandererTest.stayCLoseToHome());
+    }
+
+    @Test
+    public void clearPreexistingListsTest(){
+        when(nearbyEnemyMuckraker.get(0)).thenReturn(enemyMuck);
+        nearbyEnemyMuckraker.add(enemyMuck);
+        assertTrue(slandererTest.clearPreexistingLists());
+        assertEquals(0, nearbyEnemyMuckraker.size());
+        when(nearbyEC.get(0)).thenReturn(friendBotEC);
+        nearbyEC.add((friendBotEC));
+        assertTrue(slandererTest.clearPreexistingLists());
+        assertEquals(0, nearbyEC.size());
+    }
+
+    @Test
+    public void getNearbyEnemiesTest() throws GameActionException {
+        nearbyEnemyMuckrakerNotMock.add(enemyMuck);
+        EnemyBotInfoArr[0]=enemyMuck;
+        assertEquals(nearbyEnemyMuckrakerNotMock, slandererTest.getNearbyEnemies(EnemyBotInfoArr));
+    }
+    @Test
+    public void getNearbyAllyTest() throws GameActionException {
+        nearbyAllyECNotMock.add(friendBotEC);
+        AllyInfoArr[0]=friendBotEC;
+        assertEquals(nearbyAllyECNotMock, slandererTest.getNearbyAlly(AllyInfoArr));
+    }
+
+
+    /*@Test
     public void testTurn() throws GameActionException {
         RobotInfo bot1 = new RobotInfo(1, Team.B, RobotType.MUCKRAKER, 10, 10, new MapLocation(0, 0));
         RobotInfo bot2 = new RobotInfo(2, Team.A, RobotType.ENLIGHTENMENT_CENTER, 10, 10, new MapLocation(1, 0));
@@ -61,7 +149,7 @@ public class SlandererTest  {
         when(rcTest.canMove(Direction.NORTHWEST)).thenReturn(false);
         assertTrue(slandererTest.runFromEnemy(testLoc));
 
-    }
+    }*/
 //    @Mock
 //    private RobotInfo[] EnemyBotInfoArr = new RobotInfo[1];
 //
