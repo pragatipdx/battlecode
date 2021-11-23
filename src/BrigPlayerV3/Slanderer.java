ab6f719375accdad1ee49e8a2a740050a9bba0a7
larrisacarvalho <larris2@pdx.edu>
2021-11-23 13:37:10 -0800

    Test modifications for slanderer

--- src/BrigPlayerV3/Slanderer.java
+++ src/BrigPlayerV3/Slanderer.java
@@ -15,14 +15,11 @@ public class Slanderer extends Unit {
     public Slanderer(RobotController r) {
         super(r);
     }
-
     public Slanderer(RobotController r, Team enemy, Team friend) {
         super(r, enemy, friend);
     }
-
     public void takeTurn() throws GameActionException {
         super.takeTurn();
-        MapLocation location = rc.getLocation();
         RobotInfo[] robotInfosInSenseRadius = senseNearbyRobotsInSenseRadius();
         if(robotInfosInSenseRadius.length > 0){
 
@@ -59,23 +56,6 @@ public class Slanderer extends Unit {
     }
 
     public boolean runFromEnemy() throws GameActionException {
-        /*try{
-
-        Team enemy = rc.getTeam().opponent();
-        for (RobotInfo robot : rc.senseNearbyRobots(senseRadius, enemy)) {
-            if (robot.getType() == RobotType.MUCKRAKER) {
-                MapLocation Mlocate = robot.getLocation();
-                if (nav.moveAway(Mlocate)) {
-                    System.out.println("I ranAway!");
-                }
-            }
-        }
-    //}
-       /* catch(NullPointerException e){
-            System.out.println("NullPointerException thrown!");
-
-    }
-        return true;*/
         if(!nearbyEnemyMuckraker.isEmpty()) {
             for (RobotInfo robot : nearbyEnemyMuckraker) {
                 System.out.println("\nFound Enemy MUCKRACKER!!!!! -   MOVING   AWAY!!!!    DISTANCE: " + currentLocation.distanceSquaredTo(robot.getLocation()));
@@ -89,27 +69,7 @@ public class Slanderer extends Unit {
 
 
     public boolean stayCLoseToHome() throws GameActionException {
-        /*Team ally = rc.getTeam();
-        int actionRadiusA = rc.getType().actionRadiusSquared;
-        try {
-        for (RobotInfo robotA : rc.senseNearbyRobots(actionRadiusA, ally)) {
-                if (robotA.getType() == RobotType.ENLIGHTENMENT_CENTER) {
-                    MapLocation Mlocate = robotA.getLocation();
-                    Direction Dlocate = robotA.getLocation().directionTo(Mlocate);
-                    if (rc.canMove(Dlocate)) {
-                        if (nav.tryMove(Dlocate)) {
-                            rc.move(Dlocate);
-                            System.out.println("!!!!!!!!!I am moving towards " + Dlocate + "; " + rc.isReady() + " " + rc.getCooldownTurns() + " " + rc.canMove(Dlocate));
-                        }
 
-                    }
-                }
-            } }catch (NullPointerException e) {
-                System.out.println("NullPointerException thrown!");
-            }
-
-
-        return true; */
         if(!nearbyEC.isEmpty()) {
             for (RobotInfo robot : nearbyEC) {
                 System.out.println("\nFound Friend EC - MOVING CLOSER    DISTANCE: " + currentLocation.distanceSquaredTo(robot.getLocation()));
