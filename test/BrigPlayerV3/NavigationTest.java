bab0d9c88319a60a4f2e0cef1e4f91165aefbe91
pragatipdx <pragati@pdx.edu>
2021-11-23 23:49:47 -0800

    Test coverage to 46 - Nav

--- test/BrigPlayerV3/NavigationTest.java
+++ test/BrigPlayerV3/NavigationTest.java
@@ -13,7 +13,17 @@ public class NavigationTest {
     private RobotController rcTest = mock(RobotController.class);
     private BrigPlayerV3.Navigation rpTest = new Navigation(rcTest);
 
+     @Test
+    public void navTest() throws GameActionException {
+         assertFalse(rpTest.goTo(Direction.NORTH));
+     }
 
+     @Test
+     public void navTestGoto() throws GameActionException {
+         when(rpTest.goTo(Direction.NORTH)).thenReturn(Boolean.FALSE);
+         assertFalse(rpTest.goTo(new MapLocation(0,0)));
+
+     }
 
 
 }
