17a70c147afeef8fc3bdfe5adcad74e3fd1d7800
pragatipdx <pragati@pdx.edu>
2021-11-23 22:59:22 -0800

    Test coverage to 44

--- src/BrigPlayerV3/Communications.java
+++ src/BrigPlayerV3/Communications.java
@@ -10,55 +10,57 @@ public class Communications {
     }
 
 
-    static void sendLocation(RobotController rc) throws GameActionException {
+    boolean sendLocation(RobotController rc) throws GameActionException {
         MapLocation location = rc.getLocation();
         int x = location.x, y = location.y;
         int encodedLocation = (x % 128) * 128 + (y % 128);
         if (rc.canSetFlag((encodedLocation))) {
             rc.setFlag(encodedLocation);
         }
+        return true;
     }
 
 
-    static void sendLocationExtraInfo(RobotController rc, int extraInfo) throws GameActionException {
+    boolean sendLocationExtraInfo(RobotController rc, int extraInfo) throws GameActionException {
         MapLocation location = rc.getLocation();
         int x = location.x, y = location.y;
         int encodedLocationPlusInfo = (x % 128) * 128 + (y % 128) + extraInfo * 128 * 128;
         if (rc.canSetFlag((encodedLocationPlusInfo))) {
             rc.setFlag(encodedLocationPlusInfo);
         }
+        return true;
     }
 
 
-    static MapLocation getLocationFromFlag(RobotController rc, int flag) {
-        int y = flag % 128;
-        int x = (flag / 128) % 128;
-        int extraInformation = flag / 128 / 128;
-
-        MapLocation currentLocation = rc.getLocation();
-        int offsetX128 = currentLocation.x / 128;
-        int offsetY128 = currentLocation.y / 128;
-        MapLocation actualLocation = new MapLocation(offsetX128 * 128 + x, offsetY128 * 128 + y);
-
-        MapLocation alternative = actualLocation.translate(-128, 0);
-        if(rc.getLocation().distanceSquaredTo(alternative) < rc.getLocation().distanceSquaredTo(actualLocation)) {
-            actualLocation = alternative;
-        }
-        alternative = actualLocation.translate(128, 0);
-        if(rc.getLocation().distanceSquaredTo(alternative) < rc.getLocation().distanceSquaredTo(actualLocation)) {
-            actualLocation = alternative;
-        }
-        alternative = actualLocation.translate(0, -128);
-        if(rc.getLocation().distanceSquaredTo(alternative) < rc.getLocation().distanceSquaredTo(actualLocation)) {
-            actualLocation = alternative;
-        }
-        alternative = actualLocation.translate(0, 128);
-        if(rc.getLocation().distanceSquaredTo(alternative) < rc.getLocation().distanceSquaredTo(actualLocation)) {
-            actualLocation = alternative;
-        }
-        return actualLocation;
-
-    }
+//    static MapLocation getLocationFromFlag(RobotController rc, int flag) {
+//        int y = flag % 128;
+//        int x = (flag / 128) % 128;
+//        int extraInformation = flag / 128 / 128;
+//
+//        MapLocation currentLocation = rc.getLocation();
+//        int offsetX128 = currentLocation.x / 128;
+//        int offsetY128 = currentLocation.y / 128;
+//        MapLocation actualLocation = new MapLocation(offsetX128 * 128 + x, offsetY128 * 128 + y);
+//
+//        MapLocation alternative = actualLocation.translate(-128, 0);
+//        if(rc.getLocation().distanceSquaredTo(alternative) < rc.getLocation().distanceSquaredTo(actualLocation)) {
+//            actualLocation = alternative;
+//        }
+//        alternative = actualLocation.translate(128, 0);
+//        if(rc.getLocation().distanceSquaredTo(alternative) < rc.getLocation().distanceSquaredTo(actualLocation)) {
+//            actualLocation = alternative;
+//        }
+//        alternative = actualLocation.translate(0, -128);
+//        if(rc.getLocation().distanceSquaredTo(alternative) < rc.getLocation().distanceSquaredTo(actualLocation)) {
+//            actualLocation = alternative;
+//        }
+//        alternative = actualLocation.translate(0, 128);
+//        if(rc.getLocation().distanceSquaredTo(alternative) < rc.getLocation().distanceSquaredTo(actualLocation)) {
+//            actualLocation = alternative;
+//        }
+//        return actualLocation;
+//
+//    }
 
 
 }
--- test/BrigPlayerV3/PoliticianTest.java
+++ test/BrigPlayerV3/PoliticianTest.java
@@ -10,59 +10,91 @@ import org.junit.Test;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
+import java.util.ArrayList;
+
 
 public class PoliticianTest {
-
+    static RobotController rc;
     Team friend = Team.A;
     Team enemy = Team.B;
 
-
     private RobotController rcTest = mock(RobotController.class);
-    private BrigPlayerV3.Politician rpTest = new Politician(rcTest,enemy,friend);
+    private Politician politicianTest = new Politician(rcTest, enemy, friend);
+    int senseRadius = 10;
+    RobotInfo[] friendBotArr = new RobotInfo[1];
+   // RobotInfo[] enemyBotArr = new RobotInfo[1];
+    ArrayList<RobotInfo> nearbyAlly = new ArrayList<>();
+
+    RobotInfo friendBotSlanderer = new RobotInfo(1, friend, RobotType.SLANDERER, 10, 10, new MapLocation(1, 0));
+    RobotInfo enemyEC = new RobotInfo(2, enemy, RobotType.ENLIGHTENMENT_CENTER, 10, 10, new MapLocation(1, 0));
+
+    @Mock
+    private ArrayList<RobotInfo> nearbySlanderer;
+    private RobotInfo[] AllyInfoArr = new RobotInfo[1];
+
+    @Mock
+
+    private ArrayList<RobotInfo> nearbyEC;
+    private RobotInfo[] EnemyBotInfoArr = new RobotInfo[1];
+
+    @Before
+    public void setUp() {
+        MockitoAnnotations.initMocks(this);
+    }
+
+    @Test
+    public void politicianEmpowerTest() throws GameActionException {
+        assertTrue(politicianTest.politicianEmpower());
+    }
+
+    @Test
+    public void politicianProtector() throws GameActionException {
+        assertFalse(politicianTest.protectSlanderer());
+    }
+    @Test
+    public void senseNearbyRobotsTestNullArrayE() throws GameActionException {
+        RobotInfo[] robots = {};
+        when(rcTest.senseNearbyRobots(senseRadius, enemy)).thenReturn(robots);
+
+        assertArrayEquals(null, politicianTest.senseNearbyRobotsInSenseRadius());
+    }
+
+    @Test
+    public void getNearbyAllyTest() throws GameActionException {
+        nearbyAlly.add(friendBotSlanderer);
+        AllyInfoArr[0] = friendBotSlanderer;
+        assertEquals(nearbyAlly, politicianTest.getNearbyAlly(AllyInfoArr));
+    }
+
+    @Test
+    public void senseNearbyRobotsTestNullArrayF() throws GameActionException {
+        RobotInfo[] robots = {};
+        when(rcTest.senseNearbyRobots(senseRadius, friend)).thenReturn(robots);
+
+        assertArrayEquals(null,politicianTest.senseNearbyRobotsInSenseRadius());
+    }
+
+    @Test
+    public void senseNearbyRobotsTestArrayLen() throws GameActionException {
+        friendBotArr[0] = friendBotSlanderer;
+
+        when(rcTest.senseNearbyRobots(senseRadius)).thenReturn(friendBotArr);
+        assertEquals(friendBotArr.length, politicianTest.senseNearbyRobotsInSenseRadius().length);
+        assertArrayEquals(friendBotArr, politicianTest.senseNearbyRobotsInSenseRadius());
+    }
 
 
-    RobotInfo enemyBotMuck = new RobotInfo(2, enemy, RobotType.MUCKRAKER, 10, 10, new MapLocation(1, 0));
-
-
-//    @Mock
-//    private RobotInfo[] EnemyBotInfoArr = new RobotInfo[1];
-//
-//    @Before
-//    public void setUp() {
-//        MockitoAnnotations.initMocks(this);
-//    }
-
-
-//    @Test
-//    public void testEmpower() throws GameActionException {
-//        RobotInfo bot1 = new RobotInfo(1, Team.B, RobotType.POLITICIAN, 10, 10, new MapLocation(0, 0));
-//        RobotInfo bot2 = new RobotInfo(2, Team.A, RobotType.POLITICIAN, 10, 10, new MapLocation(1, 0));
-//        RobotInfo[] arr1 = new RobotInfo[2];
-//        arr1[0] = bot1;
-//        arr1[1] = bot2;
-//        when(rcTest.senseNearbyRobots()).thenReturn(arr1);
-//        when(rcTest.getTeam()).thenReturn(Team.A);
-//
-//        assertEquals(1, rpTest.politicianEmpower(5));
-//    }
-//
-//    @Test
-//    public void testProtectSlanderer() throws GameActionException {
-//        MapLocation testLoc = new MapLocation(10,10);
-//        when(rcTest.getType()).thenReturn(RobotType.SLANDERER);
-//        when(rcTest.getLocation()).thenReturn(new MapLocation(0,0));
-//        when(rcTest.canMove(Direction.NORTH)).thenReturn(false);
-//        when(rcTest.canMove(Direction.NORTHEAST)).thenReturn(false);
-//        when(rcTest.canMove(Direction.EAST)).thenReturn(true);
-//        when(rcTest.canMove(Direction.SOUTHEAST)).thenReturn(true);
-//        when(rcTest.canMove(Direction.SOUTH)).thenReturn(true);
-//        when(rcTest.canMove(Direction.SOUTHWEST)).thenReturn(false);
-//        when(rcTest.canMove(Direction.WEST)).thenReturn(false);
-//        when(rcTest.canMove(Direction.NORTHWEST)).thenReturn(false);
-//
-////        EnemyBotInfoArr[0]=enemyBotMuck;
-//        assertFalse(rpTest.protectSlanderer(1));
-//    }
+    @Test
+    public void clearPreexistingListsTest() {
+        when(nearbyEC.get(0)).thenReturn(enemyEC);
+        nearbyEC.add(enemyEC);
+        assertTrue(politicianTest.clearPreexistingLists());
+        assertEquals(0, nearbyEC.size());
+        when(nearbySlanderer.get(0)).thenReturn(friendBotSlanderer);
+        nearbySlanderer.add((friendBotSlanderer));
+        assertTrue(politicianTest.clearPreexistingLists());
+        assertEquals(0, nearbySlanderer.size());
+    }
 
 }
 
