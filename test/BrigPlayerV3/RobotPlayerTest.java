d76cb7cdd885aa1015ce765d4dc097883af52e65
Sam Huebschwerlen <huebschwerlen@gmail.com>
2021-11-28 14:22:27 -0800

    work on robotplayer test

--- test/BrigPlayerV3/RobotPlayerTest.java
+++ test/BrigPlayerV3/RobotPlayerTest.java
@@ -3,24 +3,30 @@ package BrigPlayerV3;
 import battlecode.common.GameActionException;
 import battlecode.common.RobotController;
 import battlecode.common.RobotType;
+import battlecode.common.Team;
 import org.junit.Ignore;
 import org.junit.Test;
 
 import static org.mockito.Mockito.*;
 
 public class RobotPlayerTest {
+
+    Team friend = Team.A;
+    Team enemy = Team.B;
+
     private RobotController rcTest = mock(RobotController.class);
 
 
     @Test
     @Ignore
-    public void ECRunTest() throws GameActionException {
+    public void muckRunTest() throws GameActionException {
 
         RobotPlayer robotPlayer = new RobotPlayer();
-//        Robot EC = new Robot(rcTest);
-//        EnlightenmentCenter EC = new EnlightenmentCenter(rcTest);
 
-        when(rcTest.getType()).thenReturn(RobotType.ENLIGHTENMENT_CENTER);
+
+        when(rcTest.getType()).thenReturn(RobotType.MUCKRAKER);
+        when(rcTest.getTeam()).thenReturn(friend);
+        when(rcTest.getTeam().opponent()).thenReturn(enemy);
 
         robotPlayer.run(rcTest);
 
